f6cb940ac1036d407e20e6621b4a3621746bf429
vknysh <tbazd@outlook.com>
2017-01-28 20:18:44 +0200

    NEW: Add getters setters

--- file-aggregator-common/src/main/java/com/file/aggregator/common/model/FileDoc.java
+++ file-aggregator-common/src/main/java/com/file/aggregator/common/model/FileDoc.java
@@ -14,4 +14,52 @@ public class FileDoc {
     private String phone;
     private int age;
     private BigDecimal tax;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
 }
